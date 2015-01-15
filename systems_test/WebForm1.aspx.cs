f68a381e394f395c781a8aea75d858d3e3be5f46
Hari Sridhar <hari.srdhr@gmail.com>
2015-01-15 11:50:18 -0500

    Added System_Test to Master Branch: Purpose is for Deployment

---
+++ systems_test/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace systems_test
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                string labelDisp = ConfigurationManager.AppSettings["string_test"].ToString();
+                Label1.Text = labelDisp;
+            }
+        }
+    }
+}
